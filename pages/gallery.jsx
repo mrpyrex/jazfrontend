792bf6324ebb3a112a24c0c13b1970ccb47e9c69
mrpyrex <ndifrkeumoren@gmail.com>
2021-04-26 11:59:27 +0100

    Added external api

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -4,7 +4,10 @@ import Meta from "../components/Meta";
 import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox";
 
 export const getStaticProps = async () => {
-  const res = await fetch("http://localhost:8000/api/gallery/");
+  const res = await fetch(
+    "https://protected-anchorage-64109.herokuapp.com/api/gallery/"
+  );
+  // const res = await fetch("http://localhost:8000/api/gallery/");
   const data = await res.json();
 
   return {
